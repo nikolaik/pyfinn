e05b01eeb0ee1cf825243115a0f3e5549eda1579
Nikolai R Kristiansen <nikolai.kristiansen@convertelligence.no>
2018-03-25 18:05:42 +0200

    Add viewings

--- finn.py
+++ finn.py
@@ -1,5 +1,6 @@
 import sys
 
+import dateparser
 from fake_useragent import UserAgent
 from requests_html import HTMLSession
 
@@ -27,6 +28,23 @@ def _list_to_vals(r, data, selector):
         data[key] = _clean(a.text)
 
 
+def _viewings(r):
+    viewings = []
+    els = r.html.find('.hide-lt768 time')
+    for el in els:
+        # Ninja parse dt range string in norwegian locale. Example: "søndag 08. april, kl. 13:00–14:00"
+        split_space = el.text.strip().split(' ')
+        if len(split_space) < 5:
+            continue
+        date, time_range = ' '.join(split_space[1:]).replace(' kl. ', '').split(',')
+        # start_hour, start_min = time_range.split('–')[0].split(':')
+        dt = dateparser.parse(date, languages=['nb'])
+        if dt:
+            # dt = dt.replace(hour=int(start_hour), minute=int(start_min))
+            viewings.append(dt.date().isoformat())
+    return viewings
+
+
 def scrape_ad(finnkode):
     url = 'https://www.finn.no/realestate/homes/ad.html?finnkode={code}'.format(code=finnkode)
     r = session.get(url, headers={'user-agent': ua.random})
@@ -38,11 +56,14 @@ def scrape_ad(finnkode):
     if not price_element or not postal_address_element:
         return
 
+    viewings = _viewings(r)
     ad_data = {
         'Postadresse': postal_address_element.text,
         'Prisantydning': _clean(price_element.text),
+        'Visningsdatoer': viewings,
         'url': url
     }
+    ad_data.update({'Visningsdato {}'.format(i): v for i, v in enumerate(viewings, start=1)})
     _list_to_vals(r, ad_data, 'h1 + p + dl + dl')
     _list_to_vals(r, ad_data, 'h1 + p + dl + dl + dl')
 
@@ -51,7 +72,7 @@ def scrape_ad(finnkode):
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
-        print('Invalid number of arguments.\n\nUsage:\n$ python finn.py URL')
+        print('Invalid number of arguments.\n\nUsage:\n$ python finn.py FINNKODE')
         exit(1)
 
     ad_url = sys.argv[1]
